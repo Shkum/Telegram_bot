126c837e8bee3844f65d5dd0bd629096c6d2f973
Sergiy <37875819+Shkum@users.noreply.github.com>
2022-07-25 00:43:41 +0300

    Update bot_run.bat

--- bot_run.bat
+++ bot_run.bat
@@ -4,7 +4,7 @@ call %~dp0venv\Scripts\activate
 
 cd %~dp0
 
-set TOKEN=5433931992:AAG7ykhximKcgFz-SY6-zjNenTVQ7X4h0dQ
+set TOKEN=5433931992:AAG7ykhximKcgFz-SY6-zjNenTVQ7X4h0d
 
 python Telegram_bot.py
 
